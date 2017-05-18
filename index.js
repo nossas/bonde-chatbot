362eadd7ee0c436a3c1b389152749849d51fcadf
Lucas Pirola <lucaspirola@gmail.com>
2017-05-18 17:08:53 -0300

    Set new url to share

--- index.js
+++ index.js
@@ -140,12 +140,12 @@ function receivedMessage (payload, reply) {
                                       "image_url": "https://scontent-gru2-2.xx.fbcdn.net/v/t1.0-9/18485498_463912997289183_6766375779007506020_n.png?oh=0d22e5dc04dbf35ea863ef3f8412eddf&oe=59A52285",
                                       "default_action": {
                                         "type": "web_url",
-                                        "url": "https://www.facebook.com/sharer.php?u=https://m.me/beta.feminista?ref=invited_by"
+                                        "url": "https://m.me/beta.feminista?ref=invited_by"
                                       },
                                       "buttons": [
                                         {
                                           "type": "web_url",
-                                          "url": "https://www.facebook.com/sharer.php?u=https://m.me/beta.feminista?ref=invited_by",
+                                          "url": "https://m.me/beta.feminista?ref=invited_by",
                                           "title": "Get started!"
                                         }
                                       ]
