5cba642e1efa77906fb8ba4d4922d91a90f7f63a
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-07-13 15:12:24 -0300

    chore(mass-message): show bots list #25

--- routes/mass-message.js
+++ routes/mass-message.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import request from 'request'
+import _ from 'underscore'
 import dateformat from 'dateformat'
 import { isAuthenticated } from './middlewares'
 import { client as graphqlClient } from '../graphql'
@@ -7,30 +8,66 @@ import * as graphqlQueries from '../graphql/queries'
 
 const router = express.Router()
 
-router.get('/', isAuthenticated, (req, res) => {
-  graphqlClient.query({
+// router.get('/', isAuthenticated, (req, res) => {
+router.get('/', (req, res) => {
+  const fetchBotConfigurations = graphqlClient.query({
+    fetchPolicy: 'network-only',
+    query: graphqlQueries.fetchBotConfigurations
+  })
+  const fetchBotRecipients = graphqlClient.query({
     fetchPolicy: 'network-only',
     query: graphqlQueries.fetchBotRecipients
   })
-    .then(({ data: { botRecipients: { recipients } } }) => {
-      res.render('./mass-message/index', { recipients, dateformat })
+
+  Promise.all([fetchBotConfigurations, fetchBotRecipients])
+    .then(values => {
+      const [botConfiguration, botRecipients] = values
+      const { data: { configs: { bots: botsRaw } } } = botConfiguration
+      const { data: { botRecipients: { recipients: recipientsRaw } } } = botRecipients
+      const date = date => dateformat(date, 'dd/mm/yyyy')
+      const time = date => dateformat(date, 'HH:MM')
+      const createAt = value => `${date(value)} às ${time(value)}`
+
+      const bots = botsRaw.map(bot => ({
+        ...bot,
+        data: JSON.parse(bot.data),
+        createdAt: createAt(bot.createdAt)
+      }))
+
+      const recipients = recipientsRaw.map(recipient => {
+        const interaction = JSON.parse(recipient.interaction)
+        return {
+          ...recipient,
+          interaction,
+          profile: interaction.profile,
+          bot: JSON.parse(recipient.facebookBotConfiguration),
+          createdAt: createAt(recipient.createdAt)
+        }
+      })
+      const appDomain = process.env.APP_DOMAIN
+
+      res.render('./mass-message/index', { bots, recipients, dateformat, appDomain })
     })
     .catch(error => console.log(`${error}`.red))
 })
 
 router.post('/send', (req, res) => {
-  const { selected_recipients: selectedRecipients, message } = req.body
+  const { selectedRecipients, message } = req.body
 
-  const promises = Object.keys(selectedRecipients).map(key => {
-    const id = String(key).replace(/\D/g, '')
-    const endpoint = `${process.env.APP_DOMAIN}/${id}/mass-message/send`
-    const recipients = selectedRecipients[key]
+  const grouping = _
+    .chain(selectedRecipients)
+    .groupBy('facebookBotConfigurationId')
+    .value()
+
+  const promises = _.toArray(_.mapObject(grouping, (value, key) => {
+    const endpoint = `${process.env.APP_DOMAIN}/${key}/mass-message/send`
+    const recipients = value.map(value => value.fbContextRecipientId)
     return request.post(endpoint, { form: { recipients, message } })
-  })
+  }))
 
-  Promise.all(promises).then(result => {
-    res.redirect('/mass-message')
-  })
+  Promise.all(promises)
+    .then(() => { res.end(JSON.stringify({ status: 'ok' })) })
+    .catch(err => console.log(`${err}`.red))
 })
 
 export default router
