972550e3bb90b5f3c62bc667a5bbdd7a10a80b90
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-08-15 12:51:12 -0300

    chore(bot): pressure using specified widget config close #22

--- bot/helpers/receive.js
+++ bot/helpers/receive.js
@@ -10,7 +10,7 @@ import * as graphqlQueries from '../../graphql/queries'
 
 export default (bot, speech, botData) => (payload, originalReply, action) => {
   bot.getProfile(payload.sender.id, (err, profile) => {
-    if (err) console.log(`${err}`.red)
+    if (err) console.error(`${err}`.red)
 
     if (process.env.SCRIPT_PATH === './scripts/v1.js') {
       switch (action) {
@@ -24,30 +24,54 @@ export default (bot, speech, botData) => (payload, originalReply, action) => {
               const [last] = interactions
               const interaction = JSON.parse(last.interaction)
 
-              const pressurePayload = {
-                "fill": {
-                  "activist": {
-                    "firstname": profile.first_name,
-                    "lastname": profile.last_name,
-                    "email": interaction.payload.message.text
-                  },
-                  "mail": {
-                    "cc": [
-                      "gabriel@nossas.org",
-                      "gabrielrtakeda@gmail.com",
-                      "gabrielrtakeda@gmail.com"
-                    ],
-                    "subject": "Pressão Feita Direto Pela API",
-                    "body": "Usando client Insomnia"
-                  }
-                }
-              }
-              request.post(
-                `${process.env.API_URL}/widgets/${botData.data.pressure_widget_id}/fill`,
-                { form: pressurePayload }
-              )
+              if (botData.data.pressure) {
+                const {
+                  custom_domain: customDomain,
+                  widget_id: widgetId,
+                  slug,
+                } = botData.data.pressure
+
+                //
+                // Retrieve the widget's data that have
+                // specified on bot's configuration
+                //
+                request({
+                  url: `${process.env.API_URL}/widgets`,
+                  qs: customDomain ? { custom_domain: customDomain } : { slug },
+                  json: true,
+                },
+                (e, r, widgets) => {
+                  if (widgets.constructor === Array) {
+                    const widget = widgets[widgets.findIndex(w => w.id === widgetId)]
+                    console.log('widget', widget)
+
+                    if (widget) {
+                      const { settings } = widget
+
+                      const activist = {
+                        firstname: profile.first_name,
+                        lastname: profile.last_name,
+                        email: interaction.payload.message.text
+                      }
+                      const mail = {
+                        "cc": settings.targets.split(';'),
+                        "subject": settings.pressure_subject,
+                        "body": settings.pressure_body
+                      }
+
+                      //
+                      // Do the pressure!
+                      //
+                      request.post(
+                        `${process.env.API_URL}/widgets/${widgetId}/fill`,
+                        { form: { fill: { activist, mail } } }
+                      )
+                    } else console.error('The widget_id specified on bot config do not match'.red)
+                  } else console.error('The API result is not an array'.red)
+                })
+              } else console.error('No pressure object defined on bot config data'.red)
             })
-            .catch(error => console.log(`${error}`.red))
+            .catch(error => console.error(`${error}`.red))
           break;
       }
     }
@@ -61,14 +85,14 @@ export default (bot, speech, botData) => (payload, originalReply, action) => {
         facebook_bot_configuration_id: botData.id,
         fb_context_recipient_id: payload.sender.id,
         fb_context_sender_id: payload.recipient.id,
-        interaction: { isBot: true, message, action }
+        interaction: { is_bot: true, message, action }
       }
       graphqlClient.mutate({
         mutation: graphqlMutations.createBotInteraction,
         variables: { interaction: JSON.stringify(interaction) }
       })
         .then(data => { originalReply(message); return data })
-        .catch(error => console.log(`${error}`.red))
+        .catch(error => console.error(`${error}`.red))
     }
 
     const message = speech.messages[action]
@@ -89,7 +113,7 @@ export default (bot, speech, botData) => (payload, originalReply, action) => {
             if (last && last.interaction) {
               const interaction = JSON.parse(last.interaction)
 
-              if (interaction.isBot) {
+              if (interaction.is_bot) {
                 switch (interaction.action) {
                   case speech.actions.QUICK_REPLY_X:
                   case speech.actions.EMAIL_ADDRESS_WRONG:
@@ -105,7 +129,7 @@ export default (bot, speech, botData) => (payload, originalReply, action) => {
               } else replyUndefined()
             } else replyUndefined()
           })
-          .catch(error => console.log(`${error}`.red))
+          .catch(error => console.error(`${error}`.red))
 
         aiClient().message(payload.message.text, {})
           .then(({ entities }) => {
