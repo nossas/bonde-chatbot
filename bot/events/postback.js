c17196e6f0631a517666414a5dfb23c1c185da4d
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-08-16 01:03:00 -0300

    fix(bot-events): pass bot data on postback event #22

--- bot/events/postback.js
+++ bot/events/postback.js
@@ -1,7 +1,7 @@
 import * as botHelpers from '../helpers'
 
 export default (bot, speech, botData) => (payload, reply) => {
-  botHelpers.receive(bot, speech)(
+  botHelpers.receive(bot, speech, botData)(
     payload,
     reply,
     payload.postback.payload
