0bde2705c87dc16b473fd1536873655a9c62497e
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-08-21 19:19:57 -0300

    chore(ai): enhance ai reply logic strategy #34

--- bot/ai/resolvers/speech-action.js
+++ bot/ai/resolvers/speech-action.js
@@ -1,16 +1,38 @@
 import _ from 'underscore'
+import { messages as aiMessages } from '../../ai'
 
 export default ({ speech, reply }) => ({ entities }) => {
-  const actionsMap = {
+  const entity = !_.isEmpty(entities) && entities.intent[0].value
+
+  //
+  // Speech specific replies
+  // (priority 1)
+  //
+  const speechRepliesMap = {
     'greeting': speech.actions.GET_STARTED,
-    'how_are_you': speech.actions.HOW_IS_IT_GOING,
+    //
+    // TODO: Need to handle via previous message context
+    //       Switch to AI intent specific replies
+    //
     'yes': speech.actions.QUICK_REPLY_B,
+  }
+  const speechReply = speechRepliesMap[entity]
+
+  //
+  // AI intent specific replies
+  // (priority 2)
+  //
+  const intentRepliesMap = {
+    'how_are_you': aiMessages.HOW_IS_IT_GOING,
     'explain_pec_29': speech.actions.QUICK_REPLY_D,
   }
+  const intentReply = !speechReply && intentRepliesMap[entity]
 
-  const entity = !_.isEmpty(entities)
-    ? actionsMap[entities.intent[0].value] || speech.actions.REPLY_UNDEFINED
-    : speech.actions.REPLY_UNDEFINED
+  //
+  // The default reply object when there is neither
+  // speech specific reply nor AI intent specific reply
+  //
+  const defaultReply = { text: aiMessages.BACK_LATER }
 
-  reply(speech.messages[entity])
+  reply(speech.messages[speechReply || intentReply] || defaultReply)
 }
