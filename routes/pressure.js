7af9c6b2e96681f0e278012993c2020ef0680f2d
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-08-22 02:48:09 -0300

    chore(pressure): render pressure widget message close #34

--- routes/pressure.js
+++ routes/pressure.js
@@ -3,10 +3,13 @@ import express from 'express'
 const router = express.Router()
 
 router.get('/:widget_id/message', (req, res) => {
-  res.render(
-    './pressure/message',
-    { pageTitle: 'Mensagem aos deputados' }
-  )
+  const { widget_id: widgetId } = req.params
+  const widget = global.widgets[widgetId]
+
+  res.render('./pressure/message', {
+    pageTitle: 'Mensagem aos deputados',
+    pressureEmailBody: (widget && widget.settings.pressure_body) || 'Pressão não encontrada!'
+  })
 })
 
 export default router
