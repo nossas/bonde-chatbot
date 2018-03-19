caf132bf1e1556e168e659f1935ada6e6c1f8b14
Camila Vilarinho <camilaavilarinho@gmail.com>
2018-03-19 17:51:35 +0000

    Add carousel

--- bot/speeches/utils/index.js
+++ bot/speeches/utils/index.js
@@ -1,11 +1,13 @@
 import * as buttonTemplate from './button-template'
 import genericTemplate from './generic-template'
+import carouselTemplate from './carousel-template'
 import quickReply from './quick-reply'
 import messageWithQuickReply from './message-with-quick-reply'
 
 export {
   buttonTemplate,
   genericTemplate,
+  carouselTemplate,
   quickReply,
   messageWithQuickReply,
 }
