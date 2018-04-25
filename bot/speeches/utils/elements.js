86308411b89603285c3e6615a416d843851e288c
Camila Vilarinho <camilaavilarinho@gmail.com>
2018-04-25 18:51:44 +0100

    update button

--- bot/speeches/utils/elements.js
+++ bot/speeches/utils/elements.js
@@ -1,7 +1,7 @@
-export default (title, subtitle, image_url, ...buttons) => ({
+export default (title, subtitle, image_url, buttons) => ({
     title,
     subtitle,
     image_url, 
-    buttons
+    buttons: buttons
   })
   
