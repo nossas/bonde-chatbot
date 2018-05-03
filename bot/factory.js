d327e3dc953b3ca4371e6d6852922b682c6995f0
Camila Vilarinho <camilaavilarinho@gmail.com>
2018-05-03 17:39:23 +0100

    update messenger-bot

--- bot/factory.js
+++ bot/factory.js
@@ -54,7 +54,7 @@ export default class BotFactory {
         const eventArgs = [bot, speech, botData]
         const endpoint = `/${data.endpoint || id}`
 
-        bot.setGetStartedButton([{ payload: speech.actions.GET_STARTED }])
+        bot.setGetStartedButton({ payload: speech.actions.GET_STARTED })
         bot.on('error', botEvents.error(...eventArgs))
         bot.on('postback', botEvents.postback(...eventArgs))
         bot.on('message', botEvents.message(...eventArgs))
