2f1e6d521dbbb33d4063627ae8d2694e7e95095f
Camila Vilarinho <camilaavilarinho@gmail.com>
2018-05-08 10:54:20 +0100

    Feature persistent menu (#124)
    * fix typo
    
    * chore(release): 2.0.0
    
    * Add persistent menu
    
    * fix user interaction error
    
    * Add persistent menu

--- bot/factory.js
+++ bot/factory.js
@@ -55,6 +55,7 @@ export default class BotFactory {
         const endpoint = `/${data.endpoint || id}`
 
         bot.setGetStartedButton({ payload: speech.actions.GET_STARTED })
+        bot.setPersistentMenu([speech.messages.PERSISTENT_MENU])
         bot.on('error', botEvents.error(...eventArgs))
         bot.on('postback', botEvents.postback(...eventArgs))
         bot.on('message', botEvents.message(...eventArgs))
