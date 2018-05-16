305c9a8e6d62ac4e0a0a4dbbfa02ac677243c556
Camila Vilarinho <camilaavilarinho@gmail.com>
2018-05-16 22:22:31 +0100

    Add deep link feature

--- bot/events/index.js
+++ bot/events/index.js
@@ -1,3 +1,4 @@
 export { default as error } from './error'
 export { default as message } from './message'
 export { default as postback } from './postback'
+export { default as referral } from './referral'
