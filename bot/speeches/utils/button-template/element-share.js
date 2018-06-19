37c89fb7977e5a193924ddd5a04a020d3a6c3d9d
Camila Vilarinho <camilaavilarinho@gmail.com>
2018-06-19 17:08:29 +0100

    Adding share with friends into the script (#168)

--- bot/speeches/utils/button-template/element-share.js
+++ bot/speeches/utils/button-template/element-share.js
@@ -1,1 +1,28 @@
-export default () => ({ type: 'element_share' })
+export default () => ({
+    type: 'element_share',
+    share_contents: {
+        attachment: {
+            type: 'template',
+            payload: {
+                template_type: 'generic',
+                elements: [{
+                    title: "A maior aliada feminista nas redes",
+                    subtitle: "Chama a Beta no inbox",
+                    image_url: 'https://goo.gl/hzZfHA',
+                    default_action: {
+                        type: 'web_url',
+                        url: "https://m.me/beta.staging"
+                    },
+                    buttons: [
+                        {
+                            type: "web_url",
+                            url: "https://m.me/beta.staging",
+                            title: "Chama a Beta no inbox"
+                        }
+                    ]
+                }
+                ]
+            }
+        }
+    }
+})
