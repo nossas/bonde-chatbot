3c755104d899c0734790d5e4c372aaf3bad2680a
Gabriel Rocha de Oliveira <rolivegab@gmail.com>
2019-06-12 13:33:39 -0300

    chore: change tab to 2 spaces

--- bot/factory.spec.js
+++ bot/factory.spec.js
@@ -3,16 +3,16 @@ import test from 'ava'
 import { BotFactory } from '.'
 
 test.beforeEach(t => {
-    const speech = require('./speeches/v2').speech
-    t.context.fabricated = new BotFactory(speech)
+  const speech = require('./speeches/v2').speech
+  t.context.fabricated = new BotFactory(speech)
 })
 
 test('fetch botConfigs', async t => {
-    const bot =  await t.context.fabricated.botConfigs()
-    t.is(typeof bot.data, 'object')
+  const bot = await t.context.fabricated.botConfigs()
+  t.is(typeof bot.data, 'object')
 })
 
 test('fabricate bot', async t => {
-    const bot =  await t.context.fabricated.fabricate()
-    t.is(typeof bot, 'object')
+  const bot = await t.context.fabricated.fabricate()
+  t.is(typeof bot, 'object')
 })
