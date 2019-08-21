ef22c1bdde198fc7fbd638293baa5dc9fe1f135d
Lucas Pirola <lucaspirola@gmail.com>
2019-08-21 11:35:28 -0300

    feat(tests): Add graphql as mock;

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,6 @@
 declare namespace NodeJS {
   export interface Global {
+    fetch: any
     widgets: any
   }
 }
--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,8 +1,45 @@
-import sayHello from '../index_old'
-import app from '../'
+import { QueryMock } from 'graphql-query-test-mock'
+import dotenv from 'dotenv'
+import { BotFactory } from '../bot'
 
-test('sayHello', () => {
-  // expect(typeof app).toBe('function')
-  expect(sayHello()).toBe('Hello, Haz!')
-  expect(sayHello('foo')).toBe('Hello, foo!')
+global.fetch = require('node-fetch')
+dotenv.config()
+
+const queryMock = new QueryMock()
+
+beforeEach(t => {
+  queryMock.setup(process.env.GRAPHQL_URL)
+  queryMock.mockQuery({
+    name: 'fetchBotConfigurations',
+    matchOnVariables: false,
+    data: {
+      configs: {
+        bots: {
+          id: '123',
+          messengerAppSecret: 'teste',
+          messengerValidationToken: 'teste',
+          messengerPageAccessToken: 'teste',
+          data: 'teste',
+          createdAt: 'teste',
+          __typename: 'bots'
+        },
+        __typename: 'bots'
+      },
+      __typename: 'bots'
+    }
+  })
+
+  const speech = require('../bot/speeches/v2')
+  this.fabricated = new BotFactory(speech.speech)
+})
+
+test('fetch botConfigs', t => {
+  expect(typeof this.fabricated).toBe('object')
+  const bot = this.fabricated.botConfigs()
+  expect(typeof bot.data).toBe('object')
+})
+
+test('fabricate bot', t => {
+  const bot = this.fabricated.fabricate()
+  expect(typeof bot).toBe('object')
 })
