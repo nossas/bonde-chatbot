432f107e5382c1f7137ad1619fed2b630452679d
Lucas Pirola <lucaspirola@gmail.com>
2019-10-01 15:56:16 -0300

    chore(devops): Fix lint

--- src/bot/ai/client.ts
+++ src/bot/ai/client.ts
@@ -1,6 +1,6 @@
 import { Wit, log } from 'node-wit'
 
 export default () => new Wit({
-  accessToken: process.env.WIT_SERVER_ACCESS_TOKEN,
+  accessToken: process.env.WIT_SERVER_ACCESS_TOKEN || '',
   logger: new log.Logger(log.DEBUG)
 })
