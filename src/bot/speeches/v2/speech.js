e03159a888c308a2b938cbc0b67ddbe30d73c0af
Lucas Pirola <lucaspirola@gmail.com>
2019-12-11 22:25:06 -0300

    fix(campaign): update share to new format

--- src/bot/speeches/v2/speech.js
+++ src/bot/speeches/v2/speech.js
@@ -492,19 +492,27 @@ export default botData => ({
     }),
     // Share campaign
     [V2_QUICK_REPLY_H_10]: [
-      genericTemplate({
-        title: 'Uma mulher que foi estuprada não pode ser obrigada a permanecer grávida! ',
-        subtitle: 'Pressione contra a PEC 29! ',
-        imageURL: 'https://s3.amazonaws.com/chatbox-beta/pec29/share-pec29.jpg',
-        buttons: [
-          buttonTemplate.shareCampaign({
-            title: 'Uma mulher que foi estuprada não pode ser obrigada a permanecer grávida! ',
-            subtitle: 'Pressione contra a PEC 29! ',
-            imageUrl: 'https://s3.amazonaws.com/chatbox-beta/pec29/share-pec29.jpg',
-            url: 'https://m.me/beta.feminista?ref=pec_29'
-          })
-        ]
-      })
+      {
+        attachment: {
+          type: 'template',
+          payload: {
+            template_type: 'media',
+            elements: [
+              {
+                media_type: 'image',
+                url: 'https://business.facebook.com/beta.staging/photos/476918122955863',
+                buttons: [
+                  {
+                    type: 'web_url',
+                    url: 'https://m.me/beta.feminista?ref=pl_3723',
+                    title: 'Pressionar deputadas(os)'
+                  }
+                ]
+              }
+            ]
+          }
+        }
+      }
     ],
     /* [V2_QUICK_REPLY_H_10]: [
       genericTemplate({
