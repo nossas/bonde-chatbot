405d0d5f6abc4551bd61af7f4cb1738e718d34c3
Lucas Pirola <lucaspirola@gmail.com>
2019-12-14 09:06:56 -0300

    fix(beta): Update how graphql client connect

--- packages/beta/src/graphql/index.ts
+++ packages/beta/src/graphql/index.ts
@@ -1,11 +1,8 @@
 import 'colors'
 import { ApolloClient } from 'apollo-client'
-import { ApolloLink, concat, split } from 'apollo-link'
+import { ApolloLink, concat } from 'apollo-link'
 import { createHttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
-// import { WebSocketLink } from 'apollo-link-ws'
-import { getMainDefinition } from 'apollo-utilities'
-// import ws from 'ws'
 import fetch from 'node-fetch'
 import dotenv from 'dotenv'
 
@@ -27,34 +24,9 @@ const authMiddleware = new ApolloLink((operation, forward) => {
   return forward(operation)
 })
 
-// Create a WebSocket link:
-// const wsLink = new WebSocketLink({
-//   uri: process.env.WS_GRAPHQL_URL || 'ws://localhost:5007/v1/graphql',
-//   options: {
-//     reconnect: true,
-//     connectionParams: { headers: authHeaders }
-//   },
-//   webSocketImpl: ws
-// })
-
-// using the ability to split links, you can send data to each link
-// depending on what kind of operation is being sent
-const link = split(
-  // split based on operation type
-  ({ query }) => {
-    const definition = getMainDefinition(query)
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    )
-  },
-  // wsLink,
-  httpLink
-)
-
 const cache = new InMemoryCache()
 
 export const client = new ApolloClient({
-  cache,
-  link: concat(authMiddleware, link)
+  link: concat(authMiddleware, httpLink),
+  cache
 })
